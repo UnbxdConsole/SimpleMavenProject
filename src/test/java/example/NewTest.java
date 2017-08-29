64d79057c276d73e8dcea7177b85c588f0de9d7d
veeru <vsalimath46@gmail.com>
2017-08-29 18:47:37 +0530

    Maven Simple project

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,25 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+static
+{
+	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+}
+	@Test
+	public void testEasy() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/selenium/guru99home/");
+		System.out.println("hisureeeee");
+
+	}
+}
